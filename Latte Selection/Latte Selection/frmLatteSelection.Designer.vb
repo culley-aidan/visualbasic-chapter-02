386ca211c714188cc0d3d79ecbbea548e9f93294
culley-aidan <54182897+culley-aidan@users.noreply.github.com>
2019-08-20 09:10:25 -0500

    updating to page 52

---
+++ Latte Selection/Latte Selection/frmLatteSelection.Designer.vb
@@ -0,0 +1,90 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmLatteSelection
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblHeading = New System.Windows.Forms.Label()
+        Me.picPumpkin = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.btnPumpkin = New System.Windows.Forms.Button()
+        CType(Me.picPumpkin, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lblHeading
+        '
+        Me.lblHeading.AutoSize = True
+        Me.lblHeading.Font = New System.Drawing.Font("Tahoma", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHeading.Location = New System.Drawing.Point(273, 61)
+        Me.lblHeading.Name = "lblHeading"
+        Me.lblHeading.Size = New System.Drawing.Size(168, 25)
+        Me.lblHeading.TabIndex = 0
+        Me.lblHeading.Text = "Latte Selection"
+        '
+        'picPumpkin
+        '
+        Me.picPumpkin.Location = New System.Drawing.Point(29, 127)
+        Me.picPumpkin.Name = "picPumpkin"
+        Me.picPumpkin.Size = New System.Drawing.Size(300, 250)
+        Me.picPumpkin.TabIndex = 1
+        Me.picPumpkin.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(385, 127)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(300, 250)
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'btnPumpkin
+        '
+        Me.btnPumpkin.Location = New System.Drawing.Point(49, 408)
+        Me.btnPumpkin.Name = "btnPumpkin"
+        Me.btnPumpkin.Size = New System.Drawing.Size(145, 40)
+        Me.btnPumpkin.TabIndex = 3
+        Me.btnPumpkin.Text = "Pumpkin Spice"
+        Me.btnPumpkin.UseVisualStyleBackColor = True
+        '
+        'frmLatteSelection
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(714, 661)
+        Me.Controls.Add(Me.btnPumpkin)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.picPumpkin)
+        Me.Controls.Add(Me.lblHeading)
+        Me.Name = "frmLatteSelection"
+        Me.Text = "Latte Selection"
+        CType(Me.picPumpkin, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblHeading As Label
+    Friend WithEvents picPumpkin As PictureBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents btnPumpkin As Button
+End Class
