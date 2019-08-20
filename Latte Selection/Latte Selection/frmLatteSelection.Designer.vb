6d7db0dd363c8b1b4e86475d396f126c08453b2b
culley-aidan <54182897+culley-aidan@users.noreply.github.com>
2019-08-20 09:30:04 -0500

    finished latte selection

--- Latte Selection/Latte Selection/frmLatteSelection.Designer.vb
+++ Latte Selection/Latte Selection/frmLatteSelection.Designer.vb
@@ -26,6 +26,11 @@ Partial Class frmLatteSelection
         Me.picPumpkin = New System.Windows.Forms.PictureBox()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.btnPumpkin = New System.Windows.Forms.Button()
+        Me.btnMocha = New System.Windows.Forms.Button()
+        Me.btnSelectLatte = New System.Windows.Forms.Button()
+        Me.lblInstructions = New System.Windows.Forms.Label()
+        Me.lblConfirmation = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
         CType(Me.picPumpkin, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
@@ -33,10 +38,10 @@ Partial Class frmLatteSelection
         'lblHeading
         '
         Me.lblHeading.AutoSize = True
-        Me.lblHeading.Font = New System.Drawing.Font("Tahoma", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblHeading.Location = New System.Drawing.Point(273, 61)
+        Me.lblHeading.Font = New System.Drawing.Font("Tahoma", 16.125!, System.Drawing.FontStyle.Bold)
+        Me.lblHeading.Location = New System.Drawing.Point(268, 60)
         Me.lblHeading.Name = "lblHeading"
-        Me.lblHeading.Size = New System.Drawing.Size(168, 25)
+        Me.lblHeading.Size = New System.Drawing.Size(178, 27)
         Me.lblHeading.TabIndex = 0
         Me.lblHeading.Text = "Latte Selection"
         '
@@ -58,18 +63,70 @@ Partial Class frmLatteSelection
         '
         'btnPumpkin
         '
-        Me.btnPumpkin.Location = New System.Drawing.Point(49, 408)
+        Me.btnPumpkin.Location = New System.Drawing.Point(91, 431)
         Me.btnPumpkin.Name = "btnPumpkin"
         Me.btnPumpkin.Size = New System.Drawing.Size(145, 40)
         Me.btnPumpkin.TabIndex = 3
         Me.btnPumpkin.Text = "Pumpkin Spice"
         Me.btnPumpkin.UseVisualStyleBackColor = True
         '
+        'btnMocha
+        '
+        Me.btnMocha.Location = New System.Drawing.Point(479, 431)
+        Me.btnMocha.Name = "btnMocha"
+        Me.btnMocha.Size = New System.Drawing.Size(145, 40)
+        Me.btnMocha.TabIndex = 4
+        Me.btnMocha.Text = "Mocha"
+        Me.btnMocha.UseVisualStyleBackColor = True
+        '
+        'btnSelectLatte
+        '
+        Me.btnSelectLatte.Location = New System.Drawing.Point(286, 431)
+        Me.btnSelectLatte.Name = "btnSelectLatte"
+        Me.btnSelectLatte.Size = New System.Drawing.Size(145, 40)
+        Me.btnSelectLatte.TabIndex = 5
+        Me.btnSelectLatte.Text = "Select Latte"
+        Me.btnSelectLatte.UseVisualStyleBackColor = True
+        '
+        'lblInstructions
+        '
+        Me.lblInstructions.AutoSize = True
+        Me.lblInstructions.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblInstructions.Location = New System.Drawing.Point(199, 515)
+        Me.lblInstructions.Name = "lblInstructions"
+        Me.lblInstructions.Size = New System.Drawing.Size(316, 14)
+        Me.lblInstructions.TabIndex = 6
+        Me.lblInstructions.Text = "Choose a latte flavor and then click Select Latte button."
+        '
+        'lblConfirmation
+        '
+        Me.lblConfirmation.AutoSize = True
+        Me.lblConfirmation.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblConfirmation.Location = New System.Drawing.Point(284, 546)
+        Me.lblConfirmation.Name = "lblConfirmation"
+        Me.lblConfirmation.Size = New System.Drawing.Size(146, 14)
+        Me.lblConfirmation.TabIndex = 7
+        Me.lblConfirmation.Text = "Enjoy your latte selection"
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(285, 584)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(145, 40)
+        Me.btnExit.TabIndex = 8
+        Me.btnExit.Text = "Exit Window"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
         'frmLatteSelection
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(714, 661)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.lblConfirmation)
+        Me.Controls.Add(Me.lblInstructions)
+        Me.Controls.Add(Me.btnSelectLatte)
+        Me.Controls.Add(Me.btnMocha)
         Me.Controls.Add(Me.btnPumpkin)
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.picPumpkin)
@@ -87,4 +144,9 @@ Partial Class frmLatteSelection
     Friend WithEvents picPumpkin As PictureBox
     Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents btnPumpkin As Button
+    Friend WithEvents btnMocha As Button
+    Friend WithEvents btnSelectLatte As Button
+    Friend WithEvents lblInstructions As Label
+    Friend WithEvents lblConfirmation As Label
+    Friend WithEvents btnExit As Button
 End Class
--- Latte Selection/Latte Selection/frmLatteSelection.vb
+++ Latte Selection/Latte Selection/frmLatteSelection.vb
@@ -1,3 +1,25 @@
 ﻿Public Class frmLatteSelection
+    Private Sub LblHeading_Click(sender As Object, e As EventArgs) Handles lblHeading.Click
 
+    End Sub
+
+    Private Sub BtnSelectLatte_Click(sender As Object, e As EventArgs) Handles btnSelectLatte.Click
+
+    End Sub
+
+    Private Sub PicPumpkin_Click(sender As Object, e As EventArgs) Handles picPumpkin.Click
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub BtnPumpkin_Click(sender As Object, e As EventArgs) Handles btnPumpkin.Click
+
+    End Sub
+
+    Private Sub BtnMocha_Click(sender As Object, e As EventArgs) Handles btnMocha.Click
+
+    End Sub
 End Class
