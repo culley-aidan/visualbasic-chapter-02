f785380fafcd76414bc575c31eb4313786ab23ed
culley-aidan <54182897+culley-aidan@users.noreply.github.com>
2019-08-20 10:30:47 -0500

    whitespace

--- Virtual Reality Headsets/Virtual Reality Headsets/Form1.vb
+++ Virtual Reality Headsets/Virtual Reality Headsets/Form1.vb
@@ -1,14 +1,19 @@
 ﻿Public Class Form1
+
     Private Sub BtnCrown_MouseClick(sender As Object, e As MouseEventArgs) Handles btnCrown.MouseClick
         btnExit.Visible = True
     End Sub
+
     Private Sub BtnEarBud_Click(sender As Object, e As EventArgs) Handles btnEarBud.Click
         btnExit.Visible = True
     End Sub
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         btnExit.Visible = False
     End Sub
+
     Private Sub BtnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
         End
     End Sub
+
 End Class
