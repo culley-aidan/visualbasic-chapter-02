fa7e82372bddb0091d4ec0b48c1bb1d2edb856b1
culley-aidan <54182897+culley-aidan@users.noreply.github.com>
2019-08-21 10:39:09 -0500

    REWRITE of Latte Selection

--- Latte Selection/Latte Selection/frmLatteSelection.Designer.vb
+++ Latte Selection/Latte Selection/frmLatteSelection.Designer.vb
@@ -23,75 +23,68 @@ Partial Class frmLatteSelection
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.lblHeading = New System.Windows.Forms.Label()
-        Me.picPumpkin = New System.Windows.Forms.PictureBox()
-        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.btnPumpkin = New System.Windows.Forms.Button()
         Me.btnMocha = New System.Windows.Forms.Button()
         Me.btnSelectLatte = New System.Windows.Forms.Button()
         Me.lblInstructions = New System.Windows.Forms.Label()
         Me.lblConfirmation = New System.Windows.Forms.Label()
         Me.btnExit = New System.Windows.Forms.Button()
+        Me.picMocha = New System.Windows.Forms.PictureBox()
+        Me.picPumpkin = New System.Windows.Forms.PictureBox()
+        CType(Me.picMocha, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.picPumpkin, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'lblHeading
         '
         Me.lblHeading.AutoSize = True
         Me.lblHeading.Font = New System.Drawing.Font("Tahoma", 16.125!, System.Drawing.FontStyle.Bold)
+        Me.lblHeading.ForeColor = System.Drawing.Color.SaddleBrown
         Me.lblHeading.Location = New System.Drawing.Point(268, 60)
         Me.lblHeading.Name = "lblHeading"
         Me.lblHeading.Size = New System.Drawing.Size(178, 27)
         Me.lblHeading.TabIndex = 0
         Me.lblHeading.Text = "Latte Selection"
         '
-        'picPumpkin
-        '
-        Me.picPumpkin.Location = New System.Drawing.Point(29, 127)
-        Me.picPumpkin.Name = "picPumpkin"
-        Me.picPumpkin.Size = New System.Drawing.Size(300, 250)
-        Me.picPumpkin.TabIndex = 1
-        Me.picPumpkin.TabStop = False
-        '
-        'PictureBox1
-        '
-        Me.PictureBox1.Location = New System.Drawing.Point(385, 127)
-        Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(300, 250)
-        Me.PictureBox1.TabIndex = 2
-        Me.PictureBox1.TabStop = False
-        '
         'btnPumpkin
         '
+        Me.btnPumpkin.BackColor = System.Drawing.Color.Bisque
+        Me.btnPumpkin.ForeColor = System.Drawing.Color.SaddleBrown
         Me.btnPumpkin.Location = New System.Drawing.Point(91, 431)
         Me.btnPumpkin.Name = "btnPumpkin"
         Me.btnPumpkin.Size = New System.Drawing.Size(145, 40)
         Me.btnPumpkin.TabIndex = 3
         Me.btnPumpkin.Text = "Pumpkin Spice"
-        Me.btnPumpkin.UseVisualStyleBackColor = True
+        Me.btnPumpkin.UseVisualStyleBackColor = False
         '
         'btnMocha
         '
+        Me.btnMocha.BackColor = System.Drawing.Color.Bisque
+        Me.btnMocha.ForeColor = System.Drawing.Color.SaddleBrown
         Me.btnMocha.Location = New System.Drawing.Point(479, 431)
         Me.btnMocha.Name = "btnMocha"
         Me.btnMocha.Size = New System.Drawing.Size(145, 40)
         Me.btnMocha.TabIndex = 4
         Me.btnMocha.Text = "Mocha"
-        Me.btnMocha.UseVisualStyleBackColor = True
+        Me.btnMocha.UseVisualStyleBackColor = False
         '
         'btnSelectLatte
         '
+        Me.btnSelectLatte.BackColor = System.Drawing.Color.Bisque
+        Me.btnSelectLatte.Enabled = False
+        Me.btnSelectLatte.ForeColor = System.Drawing.Color.SaddleBrown
         Me.btnSelectLatte.Location = New System.Drawing.Point(286, 431)
         Me.btnSelectLatte.Name = "btnSelectLatte"
         Me.btnSelectLatte.Size = New System.Drawing.Size(145, 40)
         Me.btnSelectLatte.TabIndex = 5
         Me.btnSelectLatte.Text = "Select Latte"
-        Me.btnSelectLatte.UseVisualStyleBackColor = True
+        Me.btnSelectLatte.UseVisualStyleBackColor = False
         '
         'lblInstructions
         '
         Me.lblInstructions.AutoSize = True
         Me.lblInstructions.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblInstructions.ForeColor = System.Drawing.Color.SaddleBrown
         Me.lblInstructions.Location = New System.Drawing.Point(199, 515)
         Me.lblInstructions.Name = "lblInstructions"
         Me.lblInstructions.Size = New System.Drawing.Size(316, 14)
@@ -102,20 +95,47 @@ Partial Class frmLatteSelection
         '
         Me.lblConfirmation.AutoSize = True
         Me.lblConfirmation.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblConfirmation.ForeColor = System.Drawing.Color.SaddleBrown
         Me.lblConfirmation.Location = New System.Drawing.Point(284, 546)
         Me.lblConfirmation.Name = "lblConfirmation"
         Me.lblConfirmation.Size = New System.Drawing.Size(146, 14)
         Me.lblConfirmation.TabIndex = 7
         Me.lblConfirmation.Text = "Enjoy your latte selection"
+        Me.lblConfirmation.Visible = False
         '
         'btnExit
         '
+        Me.btnExit.BackColor = System.Drawing.Color.Bisque
+        Me.btnExit.Enabled = False
+        Me.btnExit.ForeColor = System.Drawing.Color.SaddleBrown
         Me.btnExit.Location = New System.Drawing.Point(285, 584)
         Me.btnExit.Name = "btnExit"
         Me.btnExit.Size = New System.Drawing.Size(145, 40)
         Me.btnExit.TabIndex = 8
         Me.btnExit.Text = "Exit Window"
-        Me.btnExit.UseVisualStyleBackColor = True
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'picMocha
+        '
+        Me.picMocha.Image = Global.Latte_Selection.My.Resources.Resources.mocha
+        Me.picMocha.Location = New System.Drawing.Point(385, 127)
+        Me.picMocha.Name = "picMocha"
+        Me.picMocha.Size = New System.Drawing.Size(300, 250)
+        Me.picMocha.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picMocha.TabIndex = 2
+        Me.picMocha.TabStop = False
+        Me.picMocha.Visible = False
+        '
+        'picPumpkin
+        '
+        Me.picPumpkin.Image = Global.Latte_Selection.My.Resources.Resources.pumpkin1
+        Me.picPumpkin.Location = New System.Drawing.Point(29, 127)
+        Me.picPumpkin.Name = "picPumpkin"
+        Me.picPumpkin.Size = New System.Drawing.Size(300, 250)
+        Me.picPumpkin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picPumpkin.TabIndex = 1
+        Me.picPumpkin.TabStop = False
+        Me.picPumpkin.Visible = False
         '
         'frmLatteSelection
         '
@@ -128,13 +148,13 @@ Partial Class frmLatteSelection
         Me.Controls.Add(Me.btnSelectLatte)
         Me.Controls.Add(Me.btnMocha)
         Me.Controls.Add(Me.btnPumpkin)
-        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.picMocha)
         Me.Controls.Add(Me.picPumpkin)
         Me.Controls.Add(Me.lblHeading)
         Me.Name = "frmLatteSelection"
         Me.Text = "Latte Selection"
+        CType(Me.picMocha, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.picPumpkin, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -142,7 +162,7 @@ Partial Class frmLatteSelection
 
     Friend WithEvents lblHeading As Label
     Friend WithEvents picPumpkin As PictureBox
-    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents picMocha As PictureBox
     Friend WithEvents btnPumpkin As Button
     Friend WithEvents btnMocha As Button
     Friend WithEvents btnSelectLatte As Button
--- Latte Selection/Latte Selection/frmLatteSelection.vb
+++ Latte Selection/Latte Selection/frmLatteSelection.vb
@@ -1,29 +1,46 @@
-﻿Public Class frmLatteSelection
-    Private Sub LblHeading_Click(sender As Object, e As EventArgs) Handles lblHeading.Click
+﻿' Program Name: Latte Selection
+' Developer:    Aidan Culley
+' Date:         August 21st, 2019
+' Purpose:      This application allows the user to select between two lattes
 
-    End Sub
 
-    Private Sub BtnSelectLatte_Click(sender As Object, e As EventArgs) Handles btnSelectLatte.Click
-
-    End Sub
-
-    Private Sub PicPumpkin_Click(sender As Object, e As EventArgs) Handles picPumpkin.Click
-
-    End Sub
-
-    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
-
-    End Sub
+Public Class frmLatteSelection
 
     Private Sub BtnPumpkin_Click(sender As Object, e As EventArgs) Handles btnPumpkin.Click
+        ' This code executes when the Pumpkin Spice button is clicked, It will display the pumpkin picture,
+        ' hide the mocha piture and enables btnSelectLatte
 
+        picPumpkin.Visible = True
+        picMocha.Visible = False
+        btnSelectLatte.Enabled = True
     End Sub
 
     Private Sub BtnMocha_Click(sender As Object, e As EventArgs) Handles btnMocha.Click
+        ' This code executes when the Mocha button is clicked, displays mocha picture
+        ' hides pumpkin picture and enables btnSelectLatte
 
+        picMocha.Visible = True
+        picPumpkin.Visible = False
+        btnSelectLatte.Enabled = True
+    End Sub
+
+    Private Sub BtnSelectLatte_Click(sender As Object, e As EventArgs) Handles btnSelectLatte.Click
+        ' This code executes when the Select Latte button is clicked, disables pumpkin and mocha button along
+        ' with itself, will also hide the instructions.
+
+        btnPumpkin.Enabled = False
+        btnMocha.Enabled = False
+        btnSelectLatte.Enabled = False
+        lblInstructions.Visible = False
+        lblConfirmation.Visible = True
+        btnExit.Enabled = True
     End Sub
 
     Private Sub BtnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
-        End
+        ' This code executes when the Exit Window button is clicked
+        ' Exits the program
+
+        Close()
     End Sub
+
 End Class
